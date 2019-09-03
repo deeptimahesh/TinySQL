2bedc796409912e8737d1b9eb37cee11f7ab7169
deeptimahesh <deepti.mj24@gmail.com>
2019-09-04 02:06:26 +0530

    Distinct handled

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Build an SQL parser through CLI
 
 
 # TO DO : Check errors
+# done: 3, 1 
 
 import re
 import sys
@@ -126,31 +127,66 @@ def parse_query(line):
     print(columns, functions, distincts, dist_pair)
     # Now that we got all that we needed
     if len(clauses) > 1:
-        execute(columns, functions, distincts, dist_pair, needed, tables_inquery, clauses[1])
+        execute(columns, functions, distincts, dist_pair, tables_inquery, clauses[1])
     else:
-        execute(columns, functions, distincts, dist_pair, needed, tables_inquery)
+        execute(columns, functions, distincts, dist_pair, tables_inquery)
 
-def execute(columns, functions, distincts, dist_pair, needed, tables_inquery, clauses=[]):
+def execute(columns, functions, distincts, dist_pair, tables_inquery, clauses=[]):
     '''
         Execute depending on parameters passed into this function
     '''
-    if len(tables_inquery) == 1:
+    if len(dist_pair) != 0:
+        distinct_pair_process(dist_pair, tables_inquery)
+    elif len(tables_inquery) == 1:
         normal_where(clauses, columns, tables_inquery[0])
+    elif len(tables_inquery) > 1 and len(clauses) == 0:
+        join(columns, tables_inquery)
+
+def distinct_pair_process(dist_pair, tables):
+    columns_in_table = {}
+    tables_found = []
+
+    for column in [dist_pair[0][1], dist_pair[0][2]]:
+        table, column = search_column(column, tables)
+        if table not in columns_in_table.keys():
+            columns_in_table[table] = []
+            tables_found.append(table)
+        columns_in_table[table].append(column)
+    print(tables_found, columns_in_table)
+
+    data_injoin = []
+    for item1 in tables_needed[tables_found[0]]:
+        for item2 in tables_needed[tables_found[1]]:
+            data_injoin.append(item1 + item2)
+    display_output(tables_found, columns_in_table, data_injoin, join=True, distinct=True)
+
+
 
 def normal_where(clauses, columns, table):
     if len(columns) == 1 and columns[0] == '*' and len(clauses) == 0:
         columns = tables_list[table]
         print(get_headers(table, columns))
-        print("-------------------------")
+        print("-"*len(get_headers(table, columns)))
         for data in tables_needed[table]:
             ans = ''
             for column in columns:
                 ans += data[tables_list[table].index(column)] + '\t|'
             print(ans.strip('\t|'))
+    
+    elif len(clauses) == 0:
+        print(get_headers(table, columns))
+        print("-"*len(get_headers(table, columns)))
+        for row in tables_needed[table]:
+            ans = ''
+            for column in columns:
+                ans += row[tables_list[table].index(column)] + '\t|'
+            print(ans.strip('\t|'))
+    
     elif len(clauses) >= 1:
         if len(columns) == 1 and columns[0] == '*':
             columns = tables_list[table]
         print(get_headers(table, columns))
+        print("-"*len(get_headers(table, columns)))
         for row in tables_needed[table]:
             evaluator = generate_evals(row, table, clauses)
             ans = ''
@@ -158,6 +194,42 @@ def normal_where(clauses, columns, table):
                 for column in columns:
                     ans += row[tables_list[table].index(column)] + '\t|'
                 print(ans.strip('\t|'))
+
+def join_where(clauses, columns, tables):
+    operators = ['>', '<', '>=', '<=', '=']
+
+
+def join(columns, tables):
+    '''
+        Display columns from two or more tables
+    '''
+    columns_in_table = {}
+    tables_found = []
+    if len(columns) == 1 and columns[0] == '*':
+        for table in tables:
+            columns_in_table[table] = []
+            for column in tables_list[table]:
+                columns_in_table[table].append(column)
+        tables_found = tables
+    else:
+        for column in columns:
+            table, column = search_column(column, tables)
+            if table not in columns_in_table.keys():
+                columns_in_table[table] = []
+                tables_found.append(table)
+            columns_in_table[table].append(column)
+    
+    data_injoin = []
+
+    # print(tables_found, columns)
+    if len(tables_found) == 2:
+        for item1 in tables_needed[tables_found[0]]:
+            for item2 in tables_needed[tables_found[1]]:
+                data_injoin.append(item1 + item2)
+        display_output(tables_found, columns_in_table, data_injoin, join=True)
+    else:
+        display_output(tables_found, columns_in_table)
+        
 
 def get_headers(table, columns):
     string = ''
@@ -204,12 +276,57 @@ def search_column(column, tables):
             cnt += 1
             table_found = table
     if cnt > 1 or cnt == 0:
-        sys.stderr.write("Error: Column name not defined correctly\n")
+        sys.stderr.write("Error: Column name not defined correctly.\n")
         quit(-1)
     return table_found, column
 
+def display_output(tables, columns, data = tables_needed, join=False, distinct=False):
+    if distinct and join:
+        header1 = get_headers(tables[0], columns[tables[0]])
+        header2 = get_headers(tables[1], columns[tables[1]])
+        print(header1 + '|' + header2)
+        print("-"*len(header1 + '|' + header2))
+        result = []
+        for item in data:
+            ans = ''
+            for column in columns[tables[0]]:
+                ans += item[tables_list[tables[0]].index(column)] + '\t|'
+            for column in columns[tables[1]]:
+                ans += item[tables_list[tables[1]].index(column) +
+                            len(tables_list[tables[0]])] + '\t|'
+            if ans.strip('\t|') not in result:
+                result.append(ans.strip('\t|'))
+        for row in result:
+            print(row)
+            
+            
+            
+    elif join:
+        header1 = get_headers(tables[0], columns[tables[0]])
+        header2 = get_headers(tables[1], columns[tables[1]])
+        print(header1 + '|' + header2)
+        print("-"*len(header1 + '|' + header2))
+        for item in data:
+            ans = ''
+            for column in columns[tables[0]]:
+                ans += item[tables_list[tables[0]].index(column)] + '\t|'
+            for column in columns[tables[1]]:
+                ans += item[tables_list[tables[1]].index(column) +
+                            len(tables_list[tables[0]])] + '\t|'
+            print(ans.strip('\t|'))
+
+    else:
+        for table in tables:
+            print(get_headers(table, columns[table]))
+            print("-"*len(get_headers(table, columns[table])))
+            for data in data[table]:
+                ans = ''
+                for column in columns[table]:
+                    ans += data[tables_list[table].index(column)] + '\t|'
+                print(ans.strip('\t|'))
+            print("")
 
 read_metadata(META)
-query = "Select * from table1, table2 where table1.B = table2.B"
+query = "Select distinct A, D from table1, table2"
 parse_query(query)
     
