2182de3bdbb7cef952e6ceffc46f1751f5ac421b
deeptimahesh <deepti.mj24@gmail.com>
2019-09-03 19:33:11 +0530

    Single tables handled

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ Build an SQL parser through CLI
 
 '''
 
+
+# TO DO : Check errors
+
 import re
 import sys
 import csv
@@ -15,19 +18,23 @@ import csv
 # First things first : read metadata from metadata.txt
 META = './files/metadata.txt'
 tables_list = {}            # Since multiple tables
+tables_needed = {}
+
+AGG = ['distinct', 'max', 'sum', 'avg', 'min']
 
 def read_metadata(file):
     '''
         Get structure of multiple tables
     '''
     begin = 0
+
     table_name = ""
     try:
         struct_file = open(file, 'r')
     except IOError:
         sys.stderr.write("Error: No meta-structure found.\n")
         quit(-1)
-    
+
     for line in struct_file:
         line = line.strip()     # Remove white spaces
         if line == '<begin_table>':
@@ -39,22 +46,21 @@ def read_metadata(file):
             begin = 0
         elif line != "<end_table>":
             tables_list[table_name].append(line)
-    
+
     begin = 0
-    return tables_list
 
 def get_datacolumns(table_name):
     '''
         Read data in table ka file
     '''
-    file = table_name + '.csv'
+    file = './files/' + table_name + '.csv'
     data = []
     try:
-        src = open(file, 'rb')
+        src = open(file, "rt")
     except IOError:
         sys.stderr.write("Error: No corresponding .csv file found.\n")
         quit(-1)
-    
+
     info = csv.reader(src)
     for row in info:
         data.append(row)
@@ -67,20 +73,143 @@ def parse_query(line):
     '''
         Parse query
     '''
+    columns = []
+    functions = []
+    distincts = []
+    dist_pair = []
+
+    tables_inquery = []
+
     words = (re.sub(' +', ' ', line)).strip()
-   
+
     # Check for table name:
     if 'from' not in words.split():
         sys.stderr.write("Error: No table name mentioned in query.\n")
         quit(-1)
     words = words.split('from')
     stripped = [(re.sub(' +', ' ', word)).strip() for word in words]
-    clauses = stripped[1].split("where")
-
+    clauses = [(re.sub(' +', ' ', word)).strip() for word in stripped[1].split("where")]
     tables_inquery = [(re.sub(' +', ' ', word)).strip() for word in clauses[0].split(",")]
     for table in tables_inquery:
-        if table not in 
+        if table not in tables_list:
+            sys.stderr.write("Error: Table name is not valid.\n")
+            quit(-1)
+        tables_needed[table] = get_datacolumns(table)
 
-query = "Select * from table_name         , abc where hallelujah"
+    needed = [(re.sub(' +', ' ', word)).strip() for word in stripped[0][7:].split(',')]
+
+    for condition in needed:
+        if "distinct" in condition.lower():
+            dist_pair.append(['distinct', condition.split('distinct')[1].strip(), \
+                needed[(needed.index(condition))+1]])
+            continue
+        if "distinct" in needed[(needed.index(condition))-1].lower():
+            continue
+
+        part_of_agg = False
+        for func in AGG:
+            if func + '(' in condition.lower():
+                if ')' not in condition:
+                    sys.stderr.write("Error: Closing Bracket not found in query.\n")
+                    quit(-1)
+                splitter = func + '('
+                part_of_agg = True
+                if func == 'distinct':
+                    distincts.append(condition.strip(')').split(splitter)[1])
+                else:
+                    functions.append([func, condition.strip(')').split(splitter)[1]])
+                break
+        if not part_of_agg:
+            if condition != '':
+                columns.append(condition.strip('()'))
+
+    print(columns, functions, distincts, dist_pair)
+    # Now that we got all that we needed
+    if len(clauses) > 1:
+        execute(columns, functions, distincts, dist_pair, needed, tables_inquery, clauses[1])
+    else:
+        execute(columns, functions, distincts, dist_pair, needed, tables_inquery)
+
+def execute(columns, functions, distincts, dist_pair, needed, tables_inquery, clauses=[]):
+    '''
+        Execute depending on parameters passed into this function
+    '''
+    if len(tables_inquery) == 1:
+        normal_where(clauses, columns, tables_inquery[0])
+
+def normal_where(clauses, columns, table):
+    if len(columns) == 1 and columns[0] == '*' and len(clauses) == 0:
+        columns = tables_list[table]
+        print(get_headers(table, columns))
+        print("-------------------------")
+        for data in tables_needed[table]:
+            ans = ''
+            for column in columns:
+                ans += data[tables_list[table].index(column)] + '\t|'
+            print(ans.strip('\t|'))
+    elif len(clauses) >= 1:
+        if len(columns) == 1 and columns[0] == '*':
+            columns = tables_list[table]
+        print(get_headers(table, columns))
+        for row in tables_needed[table]:
+            evaluator = generate_evals(row, table, clauses)
+            ans = ''
+            if eval(evaluator):
+                for column in columns:
+                    ans += row[tables_list[table].index(column)] + '\t|'
+                print(ans.strip('\t|'))
+
+def get_headers(table, columns):
+    string = ''
+    for column in columns:
+        if string != '':
+            string += '|'
+        string += table + '.' + column
+    return string
+
+def generate_evals(row, table, clauses):
+    evaluator = ''
+    clauses = [(re.sub(' +', ' ', i)).strip() for i in clauses.split()]
+    for condition in clauses:
+        if condition == '=':
+            evaluator += condition * 2
+        elif condition.lower() == 'and' or condition.lower() == 'or':
+                evaluator += ' ' + condition.lower() + ' '
+        elif '.' in condition:
+            table_found, column = search_column(condition, [table])
+            # print(tables_list[table_here].index(column))
+            # print(row)
+            evaluator += row[tables_list[table_found].index(column)]
+        elif condition in tables_list[table]:
+            evaluator += row[tables_list[table].index(condition)]
+        else:
+            evaluator += condition
+    # print(evaluator)
+    return evaluator
+
+
+def search_column(column, tables):
+    if '.' in column:
+        table, column = column.split('.')
+        table = (re.sub(' +', ' ', table)).strip()
+        column = (re.sub(' +', ' ', column)).strip()
+        if table not in tables:
+            sys.stderr.write("Error: No such table exists.\n")
+            quit(-1)
+        return table, column
+    cnt = 0
+    table_found = ''
+    for table in tables:
+        if column in tables_list[table]:
+            cnt += 1
+            table_found = table
+    if cnt > 1 or cnt == 0:
+        sys.stderr.write("Error: Column name not defined correctly\n")
+        quit(-1)
+    return table_found, column
+
+
+read_metadata(META)
+query = "Select * from table1, table2 where table1.B = table2.B"
 parse_query(query)
     
